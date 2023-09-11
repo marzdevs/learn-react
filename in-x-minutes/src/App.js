1b90ba74ac90da1e54367998b0a11ac9ef4c5b5f
Marwa <alzokarimarwa@gmail.com>
2023-09-11 00:32:29 -0500

    final todo

--- in-x-minutes/src/App.js
+++ in-x-minutes/src/App.js
@@ -1,11 +1,28 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
-import uuidv4 from 'uuid/v4' // generates a radnom id
+import uuid from 'uuid' // generates a radnom id
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 function App() {
   // when render it will give empty todo list, todos = every single todo and setTodos is function
-  const [todos, setTodos] = useState([])
+  const [todosList, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+  // saves local  
+  useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todosList))
+  }, [todosList])
+  
+  function toggleTodo(id){
+    const newTodos = [...todosList]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = ! todo.complete
+    setTodos(newTodos)
+  }
   function handleAddTodo(e){
       const name = todoNameRef.current.value
       if (name === '') return
@@ -14,14 +31,19 @@ function App() {
       })
       todoNameRef.current.value = null // will clear input after adding to todolist
   }
+
+  function handleClearTodos(){
+    const newTodos = todosList.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
     // return to do list jsx html
     <>
-    <TodoList todoList={todos}/> 
+    <TodoList todoList={todosList} toggleTodo={toggleTodo}/> 
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}> Add Todo</button>
-    <button> Clear Complete</button>
-    <div>0 left to do</div>
+    <button onClick={handleClearTodos}> Clear Complete</button>
+    <div>{todosList.filter(todo => !todo.complete).length} left to do</div>
 
     </>
   )
--- in-x-minutes/src/TodoList.js
+++ in-x-minutes/src/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Todo from './Todo'
 // use rfc to make a function component using extension es7
-export default function TodoList({todoList}) {
+export default function TodoList({todoList, toggleTodo}) {
   return (
     todoList.map(todo => {
-        return <Todo key ={todo.id} todo={todo} />
+        return <Todo key ={todo.id} toggleTodo={toggleTodo} todo={todo} />
     })
   )
 }
